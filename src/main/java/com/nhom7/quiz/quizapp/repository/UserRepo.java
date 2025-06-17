b9182043adbba1371841d940677fdfbe2e90bafe
Nguyễn Minh <nguyenduckminh0810@gmail.com>
2025-06-18 02:50:45 +0700

    xoa src thua

--- src/main/java/com/nhom7/quiz/quizapp/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.nhom7.quiz.quizapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nhom7.quiz.quizapp.model.User;
-
-public interface UserRepo extends JpaRepository<User, Long>{
-
-}
