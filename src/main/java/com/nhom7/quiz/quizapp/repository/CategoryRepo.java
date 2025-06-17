3c9072fb8f7adc47e2ebaa930dd2206daed435ba
Nguyễn Minh <nguyenduckminh0810@gmail.com>
2025-06-18 02:35:27 +0700

    logic dang ky dang nhap va ma hoa password user, them vue

--- src/main/java/com/nhom7/quiz/quizapp/repository/CategoryRepo.java
+++
@@ -1,8 +0,0 @@
-package com.nhom7.quiz.quizapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nhom7.quiz.quizapp.model.Category;
-public interface CategoryRepo extends JpaRepository<Category, Long> {
-}
-
