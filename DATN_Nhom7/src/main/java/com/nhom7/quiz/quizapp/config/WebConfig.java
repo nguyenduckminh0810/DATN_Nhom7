53cbb9bfc44fc12842c16d3743fa3712fe0a3bf4
Nguyễn Minh <nguyenduckminh0810@gmail.com>
2025-07-28 22:31:58 +0700

    .

--- DATN_Nhom7/src/main/java/com/nhom7/quiz/quizapp/config/WebConfig.java
+++ DATN_Nhom7/src/main/java/com/nhom7/quiz/quizapp/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.nhom7.quiz.quizapp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:C:/Users/PC/Desktop/DATN_Nhom7/uploads/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // domain của Vue app
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
